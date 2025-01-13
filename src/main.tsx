a74254c6157f188619369437f4a9f0d5d7e5a148
ayoung <a01259@naver.com>
2025-01-13 17:04:17 +0900

    ✨ Feat: PWA 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import App from './App.tsx';
 import './reset.css';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('새로운 콘텐츠가 준비되었습니다.');
+  },
+  onOfflineReady() {
+    console.log('오프라인에서 사용할 준비가 완료되었습니다.');
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 import viteCompression from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,40 @@ export default defineConfig({
     viteCompression({
       algorithm: 'gzip',
       ext: '.gz',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Do It Together',
+        short_name: 'Doto',
+        description: '집안일 협동 기록 서비스, 효율적으로 집안일을 관리하세요.',
+        start_url: '/',
+        lang: 'ko',
+        background_color: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        // prefer_related_applications: true,
+        // related_applications: [
+        //   {
+        //     platform: 'play',
+        //     url: 'https://play.google.com/store/apps/details?id=com.example.app',
+        //   },
+        // ],
+        icons: [
+          {
+            src: '/doitto.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
+      },
+      devOptions: {
+        enabled: false,
+        type: 'module',
+      },
     }),
   ],
   css: {
