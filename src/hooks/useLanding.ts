fa1d12b58a8857923c210f3895010ed9e20847ac
Commeci <cjtkemfldj@naver.com>
2025-03-21 17:58:41 +0900

    ✨ Feat: 알림 관련 TEST 2

--- src/hooks/useLanding.ts
+++ src/hooks/useLanding.ts
@@ -1,18 +1,20 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getMyInitState } from '@/services/user/getMyInitState';
-import { setupPushNotifications } from '@/utils/fcm';
-import { postFcmToken } from '@/services/fcm/postFcmToken';
+import { useNotification } from '@/hooks/useNotification';
 
 export const useLanding = () => {
   const navigate = useNavigate();
+  const { initNotification } = useNotification();
 
   useEffect(() => {
     const checkInitialState = async () => {
       const accessToken = new URLSearchParams(location.search).get('access_token');
       if (accessToken) {
         sessionStorage.setItem('access_token', accessToken);
-        initNotification();
+
+        // 알림 초기화
+        await initNotification();
 
         try {
           const initState = await getMyInitState();
@@ -28,7 +30,7 @@ export const useLanding = () => {
       return;
     }
     checkInitialState();
-  }, [navigate]);
+  }, [navigate, initNotification]);
 
   const handleLogin = (provider: 'kakao' | 'google' | 'naver') => {
     window.location.href = `${
@@ -38,36 +40,5 @@ export const useLanding = () => {
     }/oauth2/authorization/${provider}`;
   };
 
-  const initNotification = async () => {
-    // 현재 알림 권한 상태 확인
-    if ('Notification' in window && Notification.permission === 'default') {
-      // 권한이 아직 요청되지 않은 경우에만 요청
-      try {
-        const permission = await Notification.requestPermission();
-        if (permission === 'granted') {
-          // 권한이 허용된 경우 FCM 설정
-          const notificationResult = await setupPushNotifications();
-          if (notificationResult) {
-            const { token, platformType } = notificationResult;
-            await postFcmToken({ token, platformType });
-          }
-        }
-      } catch (error) {
-        console.error('알림 권한 요청 오류:', error);
-      }
-    } else {
-      // 이미 권한이 허용되었거나 브라우저가 알림을 지원하지 않는 경우
-      // 기존 FCM 설정 로직 실행
-      const notificationResult = await setupPushNotifications();
-      if (notificationResult) {
-        const { token, platformType } = notificationResult;
-        try {
-          await postFcmToken({ token, platformType });
-        } catch (error) {
-          console.error('Error posting FCM token:', error);
-        }
-      }
-    }
-  };
   return { handleLogin };
 };
