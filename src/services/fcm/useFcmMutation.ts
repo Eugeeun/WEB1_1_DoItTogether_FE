0dc5a118c29a62454a4f96c9f5cd9015e67f8499
Eugeeun <jack603@naver.com>
2025-03-29 00:39:53 +0900

    ✨ Feat: FCM Mutation 추가

---
+++ src/services/fcm/useFcmMutation.ts
@@ -0,0 +1,21 @@
+import { useMutation, MutationOptions } from '@tanstack/react-query';
+import { postFcmToken } from '@/services/fcm/postFcmToken';
+import { PostTokenToServerReq, PostTokenToServerRes } from '@/types/apis/fcmApi';
+import { postFcmPush } from './postFcmPush';
+import { PushNotificationReq, PushNotificationRes } from '@/types/apis/fcmApi';
+import { deleteFcmToken } from '@/services/fcm/deleteFcmToken';
+import { DeleteTokenFromServerReq, DeleteTokenFromServerRes } from '@/types/apis/fcmApi';
+import {} from '@tanstack/react-query';
+
+// options는 사용하는 쪽에서 덮어쓰기가 가능
+export const postFcmTokenMutation = (
+  options?: MutationOptions<PostTokenToServerRes, Error, PostTokenToServerReq>
+) => useMutation({ mutationFn: postFcmToken, ...options });
+
+export const postFcmPushMutation = (
+  options?: MutationOptions<PushNotificationRes, Error, PushNotificationReq>
+) => useMutation({ mutationFn: postFcmPush, ...options });
+
+export const deleteFcmTokenMutation = (
+  options?: MutationOptions<DeleteTokenFromServerRes, Error, DeleteTokenFromServerReq>
+) => useMutation({ mutationFn: deleteFcmToken, ...options });
