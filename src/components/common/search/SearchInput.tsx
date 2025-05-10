1230a5ff7a4e8e6f6554cc3dcdd2f4ce44310082
Commeci <cjtkemfldj@naver.com>
2025-05-10 14:51:31 +0900

    ➕ Add: SearchInput 컴포넌트 추가

---
+++ src/components/common/search/SearchInput.tsx
@@ -0,0 +1,35 @@
+import { SearchIcon } from '@/components/common/icon';
+import InputBox from '@/components/common/input/InputBox';
+import { useState } from 'react';
+
+export interface SearchInputProps {}
+
+const SearchInput = ({}: SearchInputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [value, setValue] = useState('');
+
+  const handleChange = (val: string) => {
+    setValue(val);
+  };
+
+  const handleCancel = () => {
+    setValue('');
+  };
+
+  return (
+    <div className='flex items-center gap-2 rounded-lg bg-white p-2'>
+      <InputBox
+        disabled={false}
+        handleChange={handleChange}
+        leftIcon={<SearchIcon className={isFocused ? 'text-main' : 'text-gray3'} />}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      <button className='whitespace-nowrap text-black font-label' onClick={handleCancel}>
+        취소
+      </button>
+    </div>
+  );
+};
+
+export default SearchInput;
